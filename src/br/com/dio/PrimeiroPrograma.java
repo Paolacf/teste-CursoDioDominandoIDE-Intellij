18654bd599cab8570c4085a9655acae6712a540f
Paola Fonseca <fonseca.paolac@gmail.com>
2022-01-28 15:50:41 -0300

    Primeiro commit - teste

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        gato Gato = new gato();
+        System.out.println(Gato);
+
+
+    }
+}
